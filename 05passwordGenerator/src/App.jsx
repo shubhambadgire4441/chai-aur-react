32ff190574e3922b7ffef50850321703b85454af
shubhambadgire4441 <shubhambadgire@gmail.com>
2024-03-10 12:36:34 +0530

    other projects

---
+++ 05passwordGenerator/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [length , setLength ] = useState(8)
+
+  return (
+    <>
+      <h1 className='text-4xl text-center text-white'>password generator</h1>
+    </>
+  )
+}
+
+export default App
